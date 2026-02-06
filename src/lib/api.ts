a1821b9b0a5d19b3d8a9bbec3415ccae4865247e
seungmimi <seungmimi10@gmail.com>
2026-02-06 23:45:58 +0900

    ngrok-skip header 추가

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,6 +4,9 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000,
   withCredentials: false, // 쿠키 인증 쓰면 유지
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 export default api;
